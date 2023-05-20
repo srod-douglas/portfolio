2e7181cfad5d404444d6b23650604029207c16dd
srod-douglas <srod.douglas@gmail.com>
2023-05-20 05:08:41 -0300

    feat: release v1.0.1

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -1,10 +1,10 @@
-import { createStitches } from "@stitches/react";
+import { createStitches } from '@stitches/react';
 
 export const { styled } = createStitches({
   media: {
-    mobileLittle: "(max-width: 400px)",
-    mobile: "(max-width: 991px)",
-    tablet: "(max-width: 1300px)",
+    mobileLittle: '(max-width: 400px)',
+    mobile: '(max-width: 991px)',
+    tablet: '(max-width: 1300px)',
   },
   utils: {
     marginX: (value: string) => ({
--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,40 +1,33 @@
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiPostgresql, SiTypescript } from "react-icons/si";
-import { FaGit, FaPython } from "react-icons/fa";
+import { TbBrandNextjs } from 'react-icons/tb';
+import { SiPostgresql, SiTypescript, SiStyledcomponents, SiExpress, SiJirasoftware, SiFigma, SiTrello, SiJsonwebtokens } from 'react-icons/si';
+import { FaGit, FaPython } from 'react-icons/fa';
+import { DiDjango, DiScrum } from 'react-icons/di';
+
 import {
-  FaDocker,
   FaHtml5,
   FaCss3Alt,
   FaJs,
   FaNode,
   FaReact,
-} from "react-icons/fa";
+} from 'react-icons/fa';
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: FaHtml5,
-  },
-  {
-    title: "CSS",
-    img: FaCss3Alt,
-  },
-  {
-    title: "JS",
-    img: FaJs,
-  },
-  { title: "TypeScript", img: SiTypescript },
-  {
-    title: "Node JS",
-    img: FaNode,
-  },
-  {
-    title: "React",
-    img: FaReact,
-  },
-  { title: "Next.js", img: TbBrandNextjs },
-  { title: "Docker", img: FaDocker },
-  { title: "PostgreSQL", img: SiPostgresql },
-  { title: "Git", img: FaGit },
-  { title: "Python", img: FaPython },
+  { title: 'HTML5', img: FaHtml5 },
+  { title: 'CSS3', img: FaCss3Alt },
+  { title: 'JavaScript', img: FaJs },
+  { title: 'TypeScript', img: SiTypescript },
+  { title: 'React', img: FaReact },
+  { title: 'Next.js', img: TbBrandNextjs },
+  { title: 'Styled-Components', img: SiStyledcomponents },
+  { title: 'Git', img: FaGit },
+  { title: 'Node JS', img: FaNode },
+  { title: 'Express', img: SiExpress },
+  { title: 'PostgreSQL', img: SiPostgresql },
+  { title: 'JSON Web Token', img: SiJsonwebtokens },
+  { title: 'Python', img: FaPython },
+  { title: 'Django', img: DiDjango },
+  { title: 'Figma', img: SiFigma },
+  { title: 'Trello', img: SiTrello },
+  { title: 'Jira', img: SiJirasoftware },
+  { title: 'Scrum', img: DiScrum },
 ];
